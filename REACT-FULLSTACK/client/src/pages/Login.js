2e286b02452cfc5916e7c088856f6eb0e474c2d9
Sandali Jayawardhana <150525955+Sandali0726@users.noreply.github.com>
2024-12-19 19:22:02 +0530

    feat: implement authentication context and update login functionality

--- REACT-FULLSTACK/client/src/pages/Login.js
+++ REACT-FULLSTACK/client/src/pages/Login.js
@@ -1,42 +1,47 @@
-import React,{useState} from 'react'
+import React, { useState, useContext } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { useNavigation } from "react-router-dom";
+import { AuthContext } from "../helpers/AuthContext";
 
 function Login() {
-   const navigate = useNavigate();
-  const [username,setusername]=useState("");
-  const [password,setpassword]=useState("");
-  
-  const login = () => {
-    const data = {username: username, password: password};
-    axios.post("http://localhost:3001/auth/login", 
-    data).then((response) => {
-      if(response.data.error){
-        alert(response.data.error);
-      } else{
-        sessionStorage.setItem("accessToken", response.data);
-        navigate("/");}}
-      );
-  };
-  
-      
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const { setAuthState } = useContext(AuthContext);
 
+  let navigate = useNavigation();
+
+  const login = () => {
+    const data = { username: username, password: password };
+    axios.post("http://localhost:3001/auth/login", data).then((response) => {
+      if (response.data.error) {
+        alert(response.data.error);
+      } else {
+        localStorage.setItem("accessToken", response.data);
+        setAuthState(true);
+        navigate("/");
+      }
+    });
+  };
   return (
     <div className="loginContainer">
-    <label>Username:</label>
-      <input type = "text" 
-      onChange={(event) => {
-        setusername(event.target.value);
-      }}/>
+      <label>Username:</label>
+      <input
+        type="text"
+        onChange={(event) => {
+          setUsername(event.target.value);
+        }}
+      />
       <label>Password:</label>
-      <input type = "password"
-      onChange={(event) => {
-        setpassword(event.target.value);
-      }}/>
+      <input
+        type="password"
+        onChange={(event) => {
+          setPassword(event.target.value);
+        }}
+      />
 
-      <button onClick={login}>Login</button>
+      <button onClick={login}> Login </button>
     </div>
-  )
+  );
 }
 
 export default Login;
