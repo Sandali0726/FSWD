077c53f22ce754feebe8d31d8b17c505f239d6a5
Sandali Jayawardhana <150525955+Sandali0726@users.noreply.github.com>
2024-12-19 09:00:02 +0530

    feat: add bcrypt dependency and implement user registration route

--- REACT-FULLSTACK/Server/routes/Users.js
+++ REACT-FULLSTACK/Server/routes/Users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const { Users } = require("../models");
 
-router.post("/", async (req, res) => {});
+router.post("/", async (req, res) => {
+    const {username,password} = req.body;
+});
 
 module.exports = router;
