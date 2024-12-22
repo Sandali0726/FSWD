f5032c408d04fba74e50b6e5150beca8f37349ba
Sandali Jayawardhana <150525955+Sandali0726@users.noreply.github.com>
2024-12-22 12:36:55 +0530

    refactor App.js and CreatePost.js for improved readability; fix header typo in CreatePost and streamline authState management

--- REACT-FULLSTACK/client/src/pages/CreatePost.js
+++ REACT-FULLSTACK/client/src/pages/CreatePost.js
@@ -30,7 +30,7 @@ function CreatePost() {
   const navigate = useNavigate();
   const onSubmit = (data) => {
     axios.post("http://localhost:3001/posts", data,
-      {headrer: {accessToken: localStorage.getItem("accessToken")}}
+      {headers: {accessToken: localStorage.getItem("accessToken")}}
     ).then((response) => {
       navigate("/");
     });
